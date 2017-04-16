7f36496dd9ca952f38979e5634414560ca7f9988
jasonxi89 <jasonxi89@gmail.com>
2017-04-16 16:43:54 -0700

    Finish first part.
    Signed-off-by: jasonxi89 <jasonxi89@gmail.com>

--- src/BlockStack.java
+++ src/BlockStack.java
@@ -1,5 +1,38 @@
+import java.util.Stack;
+
 /**
  * Created by xichen on 2017/4/16.
  */
 public class BlockStack {
+
+    private Stack<Integer> blocks = new Stack<Integer>();
+    private int stackSize = 0;
+
+    public boolean isEmpty() {
+        return this.blocks.isEmpty();
+    }
+
+    public int peek() {
+        return this.blocks.peek();
+    }
+
+    public int pop() {
+        this.stackSize--;
+        return this.blocks.pop();
+    }
+
+    public int push(int blockNumber) {
+        this.stackSize++;
+        this.blocks.push(blockNumber);
+    }
+
+    public int get(int index) {
+        return this.blocks.get(index);
+    }
+
+    public int getStackSize() {
+        return this.stackSize;
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,33 @@
 import java.io.File;
+import java.util.Scanner;
+
 
 public class Main {
-    File file = new File("test.txt");
+    public static final String FILE_ADDRESS = "Test.txt";
     static SymbolTable symbolTable = new SymbolTable();
     static BlockStack activeBlockStack = new BlockStack();
     static BlockStack closedBlockStack = new BlockStack();
 
     public static void main(String[] args) {
-        
+
+        int currentBlockNumber = -1;
+        File thisFile = new File(FILE_ADDRESS);
+        Scanner thisScanner = new Scanner(thisFile);
+        try {
+            while (thisScanner.hasNext()) {
+                String currentWord = thisScanner.next();
+                if (currentWord.trim().toUpperCase().equals("BEGIN")) {
+                    currentBlockNumber++;
+                    activeBlockStack.push(currentBlockNumber);
+
+                }
+                if (currentWord.trim().toUpperCase().equals("END")) {
+//                        currentBlockNumber-=1;
+                }
+
+            }
+        } ;
+
 
 //        System.out.println("Hello World!");
     }
