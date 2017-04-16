14d05ceae2207236157cb586757b116fb31494fd
jasonxi89 <jasonxi89@gmail.com>
2017-04-16 15:51:20 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.io.File;
+
+public class Main {
+    File file = new File("test.txt");
+    static SymbolTable symbolTable = new SymbolTable();
+    static BlockStack activeBlockStack = new BlockStack();
+    static BlockStack closedBlockStack = new BlockStack();
+
+    public static void main(String[] args) {
+        
+
+//        System.out.println("Hello World!");
+    }
+}
---
+++ src/SymbolTableEntry.java
@@ -0,0 +1,5 @@
+/**
+ * Created by xichen on 2017/4/16.
+ */
+public class SymbolTableEntry {
+}
